449f27c1c7f5118f4aeb4a9d55b1a56e1fbfb1ff
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-12 18:06:56 +0700

    attribute, coroutine

---
+++ 3D games C# script/Assets/Script/AttributeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AttributeTest : MonoBehaviour
+{
+    public int speedPublic;
+
+    [SerializeField]
+    private int speedPrivate;
+
+    [HideInInspector]
+    public int speedHide;
+
+    [Range(0,20)]
+    public int speedRange;
+
+    Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        rb.isKinematic = false;
+
+    }
+}
---
+++ 3D games C# script/Assets/Script/Coroutine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine : MonoBehaviour
+{
+    public GameObject cube;
+    private void Start() {
+        // StartCoroutine("Test");
+        // Test2();
+        StartCoroutine("Test3", 4);
+        for(int i = 0; i < 6; i++){
+            print("Start()");
+        }
+        StopCoroutine("Test3");
+
+    }
+
+    private void Update() {
+        
+    }
+
+    IEnumerator Test(){
+        print(" Coroutine Start");
+
+        yield return new WaitForSeconds(5f);
+
+        cube.GetComponent<Renderer>().material.color = Color.red;
+
+        yield return new WaitForSeconds(2f);
+        
+        cube.GetComponent<Renderer>().material.color = Color.green;
+
+        yield return null;
+
+        print(" coroutine Ends");
+
+    }
+
+    void Test2(){
+        for(int i = 0; i < 6; i++){
+            print("Test 2()");
+        }
+    }
+
+    IEnumerator Test3(int Times){
+        for(int i = 0; i < Times; i++){
+          print("Test 3()");
+
+          yield return null;
+        }
+    }
+
+
+}
